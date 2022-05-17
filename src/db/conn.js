e2d5d1d13f9aacfa6d411b2c5c5d48b268aa2070
Sneha Mishra <sneham398@gmail.com>
2022-05-17 10:05:32 +0530

    my first backend project

---
+++ src/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/employeedata" , {
+   /* useNewUrlParser: true,   
+    useUnifiedTopology: true,
+    useCreateIndex:true*/
+}).then(() => {
+    console.log(`connection successful`)
+}).catch((err) => {
+    console.log(`no connection`)
+});
